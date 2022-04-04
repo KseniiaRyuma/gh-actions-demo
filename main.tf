a1538af0563ae1dfdab301d8e754a4e4218173ea
KseniiaRyuma <kseniya0213@gmail.com>
2022-04-04 18:02:03 -0400

    roles and schema

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
+      version = "0.30.0"
     }
   }
 
@@ -19,6 +19,6 @@ provider "snowflake" {
 }
 
 resource "snowflake_database" "demo_db" {
-  name    = "DEMO_DB"
+  name    = "ACCOUNTADMIN"
   comment = "Database for Snowflake Terraform demo"
 }
